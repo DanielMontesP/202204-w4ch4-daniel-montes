5b252cec62c333587500c62774406f6dd4080663
Daniel Montes <dmp@MacBook-Pro-de-Daniel.local>
2022-04-29 00:00:09 +0200

    Add test for info.js

--- src/components/Info/Info.test.js
+++ src/components/Info/Info.test.js
@@ -1,21 +1,20 @@
 import { render, screen } from "@testing-library/react";
-import Display from "./Display";
+import Info from "./Info";
 import PhoneContext from "../../contexts/PhoneContext";
-import React from "react-dom";
-import reactDom from "react-dom";
 
-describe("Given a Display Component", () => {
-  describe("When Display is renderized", () => {
-    test("Then it sould always find className='number'", () => {
-      const expectedText = "555";
+describe("Given a Info Component", () => {
+  describe("When Info is renderized", () => {
+    test("Then it sould always find text: Calling...", () => {
+      const expectedText = "Calling...";
+
       render(
         <PhoneContext.Provider value={""}>
-          <Display />
-          555
+          <Info />
         </PhoneContext.Provider>
       );
       const linkElement = screen.getByText(expectedText);
       expect(linkElement).toBeInTheDocument();
+      // expect(linkElement).toBeInTheDocument();
     });
   });
 });
